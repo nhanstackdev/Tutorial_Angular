54d6cd5ee662c5dcc9463c48853200011f4d8e2a
nhanstackdev <nhanstack.dev@gmail.com>
2025-09-30 15:06:33 +0700

    Lesson 2: Create Home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<h1>Hello world!</h1>`,
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent],
+  styleUrl: './app.component.css',
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="../assets/favicon.ico" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
 })
 export class AppComponent {
   title = 'homes';
