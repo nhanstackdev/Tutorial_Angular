8b7f5cf333186dd849fa3ddbfae3db7b8a479d4e
nhanstackdev <nhanstack.dev@gmail.com>
2025-10-02 15:14:39 +0700

    Lesson 6: Get data from a server

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,7 +5,7 @@ import { Hero } from '../interfaces/hero';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
