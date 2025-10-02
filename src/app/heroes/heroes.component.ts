5e5e424a7c67085f52161cfa59d31cb0f36ffbc2
nhanstackdev <nhanstack.dev@gmail.com>
2025-10-02 11:24:24 +0700

    Lesson 2: Display a selection list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mockdata/mock-heroes';
 import { Hero } from '../interfaces/hero';
 
 @Component({
@@ -7,10 +8,13 @@ import { Hero } from '../interfaces/hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
- hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
