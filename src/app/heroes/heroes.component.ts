bbf919821bbdacdd26b33f7f79781192f6c33168
nhanstackdev <nhanstack.dev@gmail.com>
2025-10-02 11:48:47 +0700

    Lesson 4: Add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mockdata/mock-heroes';
+
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 import { Hero } from '../interfaces/hero';
 
 @Component({
@@ -8,15 +10,24 @@ import { Hero } from '../interfaces/hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
+  heroes: Hero[] = [];
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
